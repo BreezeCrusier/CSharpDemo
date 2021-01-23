2eb5689b27d29ccc3398114428269e2b75fd0648
Vladimir_Lyapunov <47584292+HanyoVladScarlet@users.noreply.github.com>
2021-01-23 17:26:58 +0800

    Initialization
    The repository is set up.

---
+++ JF_Demo/JF_Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JF_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
